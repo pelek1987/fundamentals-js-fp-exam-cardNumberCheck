363be8684a677f41d636f9ee7dd208eac5ce81d6
Pawel Sioda <pawel.sioda@gmail.com>
2022-03-20 00:21:28 +0100

    finish logic of Luhn's algorithm

--- task/utils/isCardValidByLuhnsAlgorithm.js
+++ task/utils/isCardValidByLuhnsAlgorithm.js
@@ -2,5 +2,15 @@ import {normalizeCardNumber} from "./normalizeCardNumber";
 
 export const isCardValidByLuhnsAlgorithm = (cardNumber) => {
     const normalized = normalizeCardNumber(cardNumber);
-    // ... @TODO dopisać logikę algorytmu
+    const firstSet = normalized.filter((_, index) => index % 2 === 0);
+    const secondSet = normalized.filter((_, index) => index % 2 === 1);
+    const firstSum = firstSet
+        .map((n) => (n * 2).toString())
+        .reduce((total, curr) => {
+            const firstDigit = Number(curr[0]);
+            const secondDigit = curr[1] ? Number(curr[1]) : 0;
+            return total + firstDigit + secondDigit;
+        }, 0);
+    const secondSum = secondSet.reduce((total, curr) => total + curr, 0);
+    return (firstSum + secondSum) % 10 === 0;
 }
