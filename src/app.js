63957e6033f4645cd8b339a6f5e706fdcf6427b2
Pawel Sioda <pawel.sioda@gmail.com>
2022-03-20 22:49:59 +0100

    finish app/task

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import { cardNumberCheck } from './task';
 import { cardNumbers } from './task/data/cardNumbers';
 
-cardNumbers.Mastercard.map(n => cardNumberCheck(n)).forEach(res => {
+Object.values(cardNumbers).flat().map(n => cardNumberCheck(n)).forEach(res => {
     console.log('Is correct?', res[0], 'Card provider:', res[1]);
 });
